a711d447dd675f5b53130fe1c026ab058a8e41b9
Natalia Orjuela <natalia.orjuela@mail.escuelaing.edu.co>
2021-11-09 18:55:47 -0500

    configuracion shiro

---
+++ src/main/java/edu/eci/cvds/security/AnyRole.java
@@ -0,0 +1,29 @@
+package edu.eci.cvds.security;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.filter.authz.RolesAuthorizationFilter;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+public class AnyRole extends RolesAuthorizationFilter {
+
+    @Override
+    public boolean isAccessAllowed(ServletRequest request, ServletResponse response,
+                                   Object mappedValue) throws IOException {
+
+        final Subject subject = getSubject(request, response);
+        final String[] rolesArray = (String[]) mappedValue;
+        if (rolesArray == null || rolesArray.length == 0) {
+            //no roles specified, so nothing to check - allow access.
+            return true;
+        }
+
+        for (String roleName : rolesArray) {
+            if (subject.hasRole(roleName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
---
+++ src/main/java/edu/eci/cvds/security/OnlyNot.java
@@ -0,0 +1,43 @@
+package edu.eci.cvds.security;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.filter.AccessControlFilter;
+import org.apache.shiro.web.util.WebUtils;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+public class OnlyNot extends AccessControlFilter {
+    String welcomeurl;
+    @Override
+    protected boolean isAccessAllowed(ServletRequest servletRequest, ServletResponse servletResponse, Object o) throws Exception {
+        Subject subject = getSubject(servletRequest, servletResponse);
+        return !subject.isAuthenticated();
+    }
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest servletRequest, ServletResponse servletResponse) throws Exception {
+        if (SecurityUtils.getSubject().hasRole("Administrator")) {
+            welcomeurl="/faces/homeA.xhtml";
+        } else if(SecurityUtils.getSubject().hasRole("Student")){
+            welcomeurl="/faces/homeB.xhtml";
+        } else if(SecurityUtils.getSubject().hasRole("Teacher")){
+            welcomeurl="/faces/homeB.xhtml";
+        }else if(SecurityUtils.getSubject().hasRole("Graduate")){
+            welcomeurl="/faces/homeB.xhtml";
+        }else if(SecurityUtils.getSubject().hasRole("Administrative")){
+            welcomeurl="/faces/homeB.xhtml";
+        }
+
+        WebUtils.issueRedirect(servletRequest, servletResponse, welcomeurl);
+        return false;
+    }
+
+    public String getWelcomeurl() {
+        return welcomeurl;
+    }
+
+    public void setWelcomeurl(String welcomeurl) {
+        this.welcomeurl = welcomeurl;
+    }
+}
+
+
