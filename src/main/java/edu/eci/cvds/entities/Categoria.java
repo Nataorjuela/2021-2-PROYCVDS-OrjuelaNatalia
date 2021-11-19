59ca06e054e33326b772dcd9ad9fba7b494e4447
Nataorjuela <nataorjuela62@gmail.com>
2021-11-19 15:32:23 -0500

    creacion de tablas y llaves

---
+++ src/main/java/edu/eci/cvds/entities/Categoria.java
@@ -0,0 +1,11 @@
+package edu.eci.cvds.entities;
+
+public class Categoria {
+    private id;
+    private String usuarioI;
+    private String nombre;
+    private boolean estado;
+    private String correo;
+    private String contraseña;
+    private int tipoUsuario;
+}
